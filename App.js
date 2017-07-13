9755ab7f1e263de384c76fa625964ca709ad76e1
Brian Cohn <bcohn12@gmail.com>
2017-07-12 20:34:37 -0700

    working date picker

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View, Slider, Button } from 'react-native';
-
+import { StyleSheet, Text, View, Slider, Button, ScrollView } from 'react-native';
+import DatePicker from 'react-native-datepicker'
+import { addNavigationHelpers } from 'react-navigation';
 
 function upsert_slider_val(value) {
     console.log(this.state)
     this.setState(...this.state, {inter_treatment_interval_ui: value })
+}
+
+
+
+
+
+class TreatmentStartDatePicker extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <DatePicker
+        style={{width: '100%', marginTop: 50, marginBottom: 60}}
+        // date={this.state.date}
+        mode="date"
+        placeholder="Select your first treatment date"
+        format="dddd, MMMM DD, YYYY"
+        minDate={new Date()}
+        confirmBtnText="Confirm"
+        cancelBtnText="Cancel"
+        customStyles={{
+          dateInput: {
+            marginLeft: 0
+          }
+          // ... You can check the source to find the other keys.
+        }}
+        onDateChange={(date) => {
+          console.log('run child handler for ', date);
+          this.props.handler(date);
+          // this.props.render();
+        }}
+      />
+    )
+  }
 }
 
 export default class App extends React.Component {
@@ -12,31 +49,37 @@ export default class App extends React.Component {
     super(props);
     //instantiate the state for the UI vals
     let rightNow = new Date()
-    this.state = {inter_treatment_interval_ui: 2,  treatment_num_ui: 12, start_date: rightNow};
+    this.state = {inter_treatment_interval_ui: 2,  treatment_num_ui: 12, treatment_start_date: rightNow};
+    this.handler = this.handler.bind(this)
+  }
+  handler(date) {
+    console.log('handler run with ', date)
+    this.setState(...this.state, {date: date})
   }
   render() {
     return (
       <View style={styles.container}>
-<View style={styles.treatment_option_slider_card}>
-      <Text style={styles.my_font, styles.card_header_text}>{this.state.inter_treatment_interval_ui} Weeks Between Treatments</Text>
-        <Slider step={1} minimumValue={1} maximumValue={4} value={2}
-          onValueChange={(value) => {
-            this.setState(...this.state, {inter_treatment_interval_ui: value })
-        }}
-        />
-<Text style={styles.my_font, styles.card_header_text}>{this.state.treatment_num_ui} Treatments</Text>
-        <Slider step={1} minimumValue={1} maximumValue={20} value={12}
-        onValueChange={(value) => {
-          this.setState(...this.state, {treatment_num_ui: value })
-      }}
-              />
 
-              <Button
-                onPress={() => (console.log(this.state,"dispatch(hideTreatmentSettings(this.state.inter_treatment_interval_ui, this.state.treatment_num_ui))"))}
-                title="Done"
-                color="#333"
-              />
-</View>
+      <View style={styles.treatment_option_slider_card}>
+      <Text style={styles.my_font, styles.tx_settings_header}>{this.state.treatment_num_ui} Treatments</Text>
+              <Slider step={1} minimumValue={1} maximumValue={20} value={12}
+              onValueChange={(value) => {
+                this.setState(...this.state, {treatment_num_ui: value })
+            }}
+                    />
+                    <Text style={styles.my_font, styles.tx_settings_header}>{this.state.inter_treatment_interval_ui} Weeks Between Treatments</Text>
+                    <Slider step={1} minimumValue={1} maximumValue={4} value={2}
+                    onValueChange={(value) => {
+                      this.setState(...this.state, {inter_treatment_interval_ui: value })
+                    }}
+                    />
+                    <TreatmentStartDatePicker handler={this.handler}/>
+                    <Button
+                      onPress={() => (console.log(this.state,"dispatch(hideTreatmentSettings(this.state.inter_treatment_interval_ui, this.state.treatment_num_ui))"))}
+                      title="Done"
+                      color="#333"
+                    />
+      </View>
 
           <Text style={styles.my_font, styles.card_header_text}>NEXT TREATMENT DATE</Text>
               <Text style={styles.my_font}>Friday April 19, 2017</Text>
@@ -50,10 +93,10 @@ export default class App extends React.Component {
               <Text style={styles.my_font, styles.card_header_text}>LATER THIS WEEK</Text>
               <Text style={styles.my_font, styles.normal_body_text}>Vomiting and nausea for two more days, expected to feel near-normal on Tuesday</Text>
               <View style={{flexDirection: 'row', margin:20}}>
-                <Text style={styles.my_font,{flex:1, width:20, fontSize: 8}}>FATIGUE</Text>
-                <Text style={styles.my_font,{flex:1, width:20, fontSize: 8}}>NAUSEA</Text>
-                <Text style={styles.my_font,{flex:1, width:20, fontSize: 8}}>PAIN</Text>
-                <Text style={styles.my_font,{flex:1, width:20, fontSize: 8}}>NEUROPATHY</Text>
+              <Text style={styles.my_font,{flex:1, width:20, fontSize: 8}}>FATIGUE</Text>
+              <Text style={styles.my_font,{flex:1, width:20, fontSize: 8}}>NAUSEA</Text>
+              <Text style={styles.my_font,{flex:1, width:20, fontSize: 8}}>PAIN</Text>
+              <Text style={styles.my_font,{flex:1, width:20, fontSize: 8}}>NEUROPATHY</Text>
               </View>
 
             </View>
@@ -66,14 +109,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height:'100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   treatment_option_slider_card: {
     width:'100%',
-    marginLeft: 10,
-    marginRight:10,
+    padding:30,
+    marginLeft: 20,
+    marginRight:20,
     backgroundColor: 'lightgrey',
   },
   my_font: {
@@ -85,5 +130,10 @@ const styles = StyleSheet.create({
   card_header_text: {
     fontSize: 14,
     fontWeight: 'bold'
+  },
+  tx_settings_header: {
+    fontSize: 30,
+    marginBottom: 30,
+    marginTop: 50,
   }
 });
